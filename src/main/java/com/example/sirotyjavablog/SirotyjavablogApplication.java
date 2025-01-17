704afd60d4425edd15c68530e716f33f1077f780
aslylit <asylaidana28@gmail.com>
2025-01-17 23:05:06 +0500

    Initial commit

---
+++ src/main/java/com/example/sirotyjavablog/SirotyjavablogApplication.java
@@ -0,0 +1,13 @@
+package com.example.sirotyjavablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirotyjavablogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirotyjavablogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sirotyjavablog/SirotyjavablogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sirotyjavablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirotyjavablogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
